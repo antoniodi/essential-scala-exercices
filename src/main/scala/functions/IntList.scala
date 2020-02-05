b9d79b3cdd2bd0ff253e9e259df2be65563e2337
antoniococr <anthonydicortes@gmail.com>
2020-02-04 20:18:27 -0500

    example of how to use fold in a list

---
+++ src/main/scala/functions/IntList.scala
@@ -0,0 +1,42 @@
+package functions
+
+trait IntList {
+
+  def fold[A](end: A, f: (Int, A) => A): A =
+    this match {
+      case End() => end
+      case Pair(head, tail) => f(head, tail.fold(end, f))
+    }
+
+  def sum: Int = {
+    this.fold[Int](0, (head, tail) => head + tail)
+  }
+
+  def length: Int = {
+    this.fold[Int](0, (_, tail) => 1 + tail)
+  }
+
+  def product: Int = {
+    this.fold[Int](1, (head, tail) => head * tail)
+  }
+
+  def double: IntList = {
+    this.fold[IntList](End(), (head, tail) => Pair(head * 2, tail) )
+  }
+
+}
+
+case class End() extends IntList
+case class Pair( heap: Int, tail: IntList) extends IntList
+
+object main{
+
+  def main(args: Array[String]): Unit = {
+    val intList = Pair(10, Pair(15, Pair(7, Pair(2, End()))))
+
+    println(intList.sum)
+    println(intList.product)
+    println(intList.length)
+    print(intList.double)
+  }
+}
